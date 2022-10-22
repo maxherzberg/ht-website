506cf71942f7dad66a2a84998fc31a3d242074c6
jejahn <55805931+jejahn@users.noreply.github.com>
2022-10-22 09:28:43 +0200

    Added hardcoded users
    Basic user functionality for further testing

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ function login() {
     var user = document.getElementById("user").value;
     var pass = document.getElementById("password").value;
 
-    alert(user + " " + pass);
+    if(user == "admin" && pass == "admin") {
+        location.href = "admin.html";
+    } else if (user == "default" && pass == "default") {
+        location.href = "default.html";
+    } else {
+        alert("Invalid credentials!");
+        document.getElementById("user").value = "";
+        document.getElementById("password").value = "";
+    }
+}
+
+function logout() {
+    location.href = "index.html";
 }
